4bff4d66f6a24ac6e15b5196559eb11a1309d98f
Dmitry Rogozhkin <dmitry@unioninformatics.com>
2019-09-18 19:13:28 -0400

    Chart component wip

---
+++ src/ui.controls/chart.js
@@ -0,0 +1,13 @@
+define([
+    'require',
+    '../inheritance',
+    '../component',
+    '../event',
+	'../view',
+	'!chart.css',
+	'!chart.html',
+],function(require,inheritance,component,event,element){
+	"use strict"
+
+
+});
