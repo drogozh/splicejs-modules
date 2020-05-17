12adbe1b3b8c9355ff69ba7c615fc4a77d0781fb
Dmitry Rogozhkin <dmitry@unioninformatics.com>
2020-05-17 02:29:51 -0400

    Added some event api

--- lib/events.d.ts
+++ lib/events.d.ts
@@ -5,8 +5,8 @@ export interface Subscription {
 export interface Event<T = any> {  
     (...args: T[]) : void;
     subscribe(callback:(...args: T[]) => void) : Subscription;
-    unsubscribe(subscription:Subscription): void;
-    dispose():void;
+    unsubscribe(subscription:Subscription) : void;
+    dispose() : void;
 }
 
 interface  EventConstructor<T=any> {
