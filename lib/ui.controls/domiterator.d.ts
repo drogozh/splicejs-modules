ca0847c5dbd60e7fdd0f18075db9152777a2c9d8
Dmitry Rogozhkin <dmitry@unioninformatics.com>
2020-11-15 13:36:39 -0500

    Update util types

--- lib/ui.controls/domiterator.d.ts
+++ lib/ui.controls/domiterator.d.ts
@@ -4,4 +4,8 @@ import {Event} from '../events';
 export default class DomIterator extends ComponentBase {
     static Component: Component<DomIterator>;
     onDataUpdated: Event;
-} 
+}
+
+export interface ItemSelectedEventArgs {
+    data: any
+}
--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,2 +1,3 @@
 export function filter<T>(collection:T[],callback:(args:any)=>any) : T[];
 export function unique<T>(collection:T[],callback:(args:T) => T): T[];
+export function assign(...objects:any):any;
